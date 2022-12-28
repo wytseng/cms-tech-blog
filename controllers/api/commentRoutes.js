869f1dff62a0a179050cf4fcc7d4fc0fdeba1c1f
Steph T <wytseng@uci.edu>
2022-12-27 20:44:08 -0800

    comment api post functioning

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,8 +4,8 @@ const withAuth = require('../../utils/auth');
 
 comments.post('/', withAuth, async (req, res) => {
   try {
-    const commentData = await Comment.create({ ...req.body, userId: req.session.userId});
-    res.status(200).json(commentData);
+    const newComment = await Comment.create({ ...req.body, userId: req.session.userId});
+    res.status(200).json(newComment);
   } catch(err) {
     console.log(err);
     res.status(400).json(err);
